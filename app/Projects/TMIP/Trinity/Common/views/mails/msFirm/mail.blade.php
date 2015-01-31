4d96b38aa85d419ffe9e57c151eeec27b5a61fdd
final.lee <final.lee@thecorporation.co.kr>
2015-02-01 05:25:13 +0900

    중소기업 공동구매 신청 페이지 완료

--- app/Projects/TMIP/Trinity/Common/views/mails/msFirm/mail.blade.php
+++ app/Projects/TMIP/Trinity/Common/views/mails/msFirm/mail.blade.php
@@ -1,9 +1,10 @@
-{{ $company['name'] }}의 {{ $company['applicant_name'] }} 담당자님!
-
-더만다린 중소기업 특화교육에 신청하여 주셔서 감사드립니다!
-
-신청한 내용은 접수되었으며 담당 컨설턴트가 리뷰 후 신속하게 연락드리겠습니다.
-
-감사합니다.
-
-추가 문의사항은 <a href="mailto:help@themandarin.co.kr">help@themandarin.co.kr</a> 로 연락주시기 바랍니다.
+{{ $company['name'] }}의 {{ $company['applicant_name'] }} 담당자님!<br/>
+<br/>
+더만다린 중소기업 특화교육에 신청하여 주셔서 감사드립니다!<br/>
+<br/>
+신청한 내용은 접수되었으며 담당 컨설턴트가 리뷰 후 신속하게 연락드리겠습니다. <br/>
+<br/>
+감사합니다. <br/>
+<br/>
+추가 문의사항은 <a href="mailto:help@themandarin.co.kr">help@themandarin.co.kr</a> 로 연락주시기 바랍니다. <br/>
+<br/>
