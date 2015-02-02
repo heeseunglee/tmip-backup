d2c67d489314951e9cec272a04808837b9456187
final.lee <final.lee@thecorporation.co.kr>
2015-02-02 11:36:57 +0900

    이메일 문구 변

--- app/Projects/TMIP/Trinity/Common/views/mails/msFirm/mail.blade.php
+++ app/Projects/TMIP/Trinity/Common/views/mails/msFirm/mail.blade.php
@@ -1,10 +1,20 @@
-{{ $company['name'] }}의 {{ $company['applicant_name'] }} 담당자님!<br/>
+안녕하십니까? {{ $company['name'] }} {{ $company['applicant_deputy'] }} {{ $company['applicant_name'] }} {{ $company['applicant_position'] }}님. <br/>
 <br/>
-더만다린 중소기업 특화교육에 신청하여 주셔서 감사드립니다!<br/>
+B2B 중국어 HRD 컨설팅기업 더만다린 교육관리팀 입니다.<br/>
 <br/>
-신청한 내용은 접수되었으며 담당 컨설턴트가 리뷰 후 신속하게 연락드리겠습니다. <br/>
+더만다린 중국지역전문가 양성교육에 참여해 주셔서 감사합니다.<br/>
 <br/>
-감사합니다. <br/>
+교육 신청은 정상적으로 처리되었으며<br/>
 <br/>
-추가 문의사항은 <a href="mailto:help@themandarin.co.kr">help@themandarin.co.kr</a> 로 연락주시기 바랍니다. <br/>
+전담 컨설턴트를 통해 신속하게 상세 교육 안내 진행하도록 하겠습니다.<br/>
 <br/>
+기타 문의사항 및 요청사항이 있으실경우<br/>
+<br/>
+<a href="mailto:help@themandarin.co.kr">help@themandarin.co.kr</a> 로 언제든지 연락주시면 감사드리겠습니다.<br/>
+<br/>
+좋은하루되세요.<br/>
+<br/>
+감사합니다.<br/>
+<br/>
+<br/>
+The Mandarin 교육관리팀 배상<br/>
